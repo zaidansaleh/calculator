ec9bb240110be9d5b6607938d2fa17d3f5259602
Saleh Zaidan <salehzaidan64@gmail.com>
2025-05-09 06:22:20 +0700

    Implement AST parser

--- calculator.c
+++ calculator.c
@@ -1,5 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef enum {
     TOKEN_TYPE_NUMBER,
@@ -75,15 +77,208 @@ Token lexer_next(Lexer *lexer) {
     return token;
 }
 
+typedef enum {
+    AST_NODE_NUMBER,
+    AST_NODE_BINARY,
+} ASTNodeType;
+
+typedef struct ASTNode {
+    ASTNodeType type;
+    union {
+        struct {
+            double value;
+        } number;
+        struct {
+            Token op;
+            struct ASTNode *left;
+            struct ASTNode *right;
+        } binary;
+    };
+} ASTNode;
+
+ASTNode *ast_node_new() {
+    ASTNode *node = malloc(sizeof(ASTNode));
+    if (!node) {
+        return NULL;
+    }
+    return node;
+}
+
+void ast_node_free(ASTNode *node) {
+    if (node->type == AST_NODE_BINARY) {
+        ast_node_free(node->binary.left);
+        ast_node_free(node->binary.right);
+    }
+    free(node);
+}
+
+void ast_node_pprint(const ASTNode *node, int indent, FILE *stream) {
+    for (int i = 0; i < 2 * indent; ++i) {
+        fputc(' ', stream);
+    }
+
+    switch (node->type) {
+    case AST_NODE_NUMBER:
+        fprintf(stream, "%g\n", node->number.value);
+        break;
+    case AST_NODE_BINARY:
+        token_println(&node->binary.op, stream);
+        ast_node_pprint(node->binary.left, indent + 1, stream);
+        ast_node_pprint(node->binary.right, indent + 1, stream);
+        break;
+    }
+}
+
+typedef struct {
+    Lexer lexer;
+    Token current;
+    const char *errmsg;
+} Parser;
+
+Parser parser_new(const char *input) {
+    Lexer lexer = {.pos = input};
+    Token current = lexer_next(&lexer);
+    Parser parser = {.lexer = lexer, .current = current, .errmsg = NULL};
+    return parser;
+}
+
+ASTNode *parse_start(Parser *parser);
+ASTNode *parse_expr(Parser *parser);
+ASTNode *parse_term(Parser *parser);
+ASTNode *parse_number(Parser *parser);
+
+ASTNode *parse_start(Parser *parser) {
+    ASTNode *root = parse_expr(parser);
+    if (parser->current.type != TOKEN_TYPE_EOF) {
+        parser->errmsg = "invalid expression";
+    }
+    return root;
+}
+
+ASTNode *parse_expr(Parser *parser) {
+    ASTNode *left = parse_term(parser);
+    if (!left) {
+        return NULL;
+    }
+
+    while (parser->current.type == TOKEN_TYPE_PLUS ||
+            parser->current.type == TOKEN_TYPE_MINUS) {
+        Token op = parser->current;
+        parser->current = lexer_next(&parser->lexer);
+
+        ASTNode *right = parse_term(parser);
+        if (!right) {
+            return left;
+        }
+
+        ASTNode *node = ast_node_new();
+        if (!node) {
+            parser->errmsg = "failed to allocate memory";
+            return left;
+        }
+
+        node->type = AST_NODE_BINARY;
+        node->binary.op = op;
+        node->binary.left = left;
+        node->binary.right = right;
+
+        left = node;
+    }
+
+    return left;
+}
+
+ASTNode *parse_term(Parser *parser) {
+    ASTNode *left = parse_number(parser);
+    if (!left) {
+        return NULL;
+    }
+
+    while (parser->current.type == TOKEN_TYPE_STAR ||
+            parser->current.type == TOKEN_TYPE_SLASH) {
+        Token op = parser->current;
+        parser->current = lexer_next(&parser->lexer);
+
+        ASTNode *right = parse_number(parser);
+        if (!right) {
+            return left;
+        }
+
+        ASTNode *node = ast_node_new();
+        if (!node) {
+            parser->errmsg = "failed to allocate memory";
+            return left;
+        }
+
+        node->type = AST_NODE_BINARY;
+        node->binary.op = op;
+        node->binary.left = left;
+        node->binary.right = right;
+
+        left = node;
+    }
+
+    return left;
+}
+
+ASTNode *parse_number(Parser *parser) {
+    if (parser->current.type != TOKEN_TYPE_NUMBER) {
+        parser->errmsg = "expected number";
+        return NULL;
+    }
+
+    Token token = parser->current;
+    parser->current = lexer_next(&parser->lexer);
+
+    char *tmp = malloc(sizeof(char) * (token.length + 1));
+    if (!tmp) {
+        parser->errmsg = "failed to allocate memory";
+        return NULL;
+    }
+
+    memcpy(tmp, token.start, token.length);
+    tmp[token.length] = '\0';
+
+    char *endptr;
+    double value = strtod(tmp, &endptr);
+    if (endptr == tmp || *endptr != '\0') {
+        parser->errmsg = "invalid number";
+        free(tmp);
+        return NULL;
+    }
+
+    ASTNode *node = ast_node_new();
+    if (!node) {
+        parser->errmsg = "failed to allocate memory";
+        free(tmp);
+        return NULL;
+    }
+
+    node->type = AST_NODE_NUMBER;
+    node->number.value = value;
+
+    free(tmp);
+    return node;
+}
+
 int main(void) {
     const char *input = "1 + 2 - 3 * 4 / 5";
 
-    Lexer lexer = {.pos = input};
-    Token token;
-    while ((token = lexer_next(&lexer)).type != TOKEN_TYPE_EOF) {
-        token_println(&token, stdout);
+    Parser parser = parser_new(input);
+    ASTNode *root = parse_start(&parser);
+    if (parser.errmsg) {
+        fprintf(stderr, "error: %s\n", parser.errmsg);
+        if (root) {
+            ast_node_free(root);
+        }
+        return 1;
     }
 
+    ast_node_pprint(root, 0, stdout);
+
+    if (root) {
+        ast_node_free(root);
+    }
     return 0;
 }
 
